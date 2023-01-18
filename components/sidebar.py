55bd948063fe7d4330e17118661d31bebe1a99e8
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2023-01-18 15:45:19 -0300

    base pronta

--- components/sidebar.py
+++ components/sidebar.py
@@ -12,16 +12,18 @@ import pandas as pd
 
 # ========= Layout ========= #
 layout = dbc.Col([
-        html.H1('Venda de games', className='text-primary'),
-        html.P('By Tópicos Especiais de Info', className='text-info'),
-        html.Hr(),
+        dbc.Card([
+                html.H1('Lista de Filmes', className='text-primary'),
+                html.P('By Tópicos Especiais de Info', className='text-info'),
+                html.Hr(),
 
-#Seção perfil
-        dbc.Button(id='botao_avatar',
-        children=[html.Img(src='/assets/Game-Icon.png', id='avatar_change', alt='Avatar', className='perfil_avatar')], 
-        style={'background-color':'transparent', 'border-color':'transparent', 'margin-left':'18%'}),
-        html.Button('Acessar dashboard', className='button-git', style={'margin-left':'15%'}),
-        ], id='sidebar_completa', style={'border':'1px solid #c4c4c4'})
+        #Seção perfil
+                dbc.Button(id='botao_avatar',
+                children=[html.Img(src='/assets/Game-Icon.png', id='avatar_change', alt='Avatar', className='perfil_avatar')], 
+                style={'background-color':'transparent', 'border-color':'transparent'}),
+                html.Button('Acessar dashboard', className='button-git', style={'margin-left':'15%'}),
+        ], id='sidebar_completa', style={'margin-top':'10px', 'text-align':'center'}),
+        ])
 
 
 
